ff8a7832ebb8ee272ffec0e0105f11974c18be78
Vincenzo Tramo <vv.tramo@gmail.com>
2023-05-07 11:48:14 +0200

    (Feat 1 Iteration 1): add a failing unit test

--- src/main/java/it/unina/softeng/tdd/demo/keepmovies/KeepMovies.java
+++ src/main/java/it/unina/softeng/tdd/demo/keepmovies/KeepMovies.java
@@ -1,5 +1,10 @@
 package it.unina.softeng.tdd.demo.keepmovies;
 
+import java.util.Set;
+
 public class KeepMovies {
 
+  public Set<Movie> getMovies() {
+    return null;
+  }
 }
--- src/test/java/it/unina/softeng/tdd/demo/keepmovies/KeepMoviesSpecification.java
+++ src/test/java/it/unina/softeng/tdd/demo/keepmovies/KeepMoviesSpecification.java
@@ -1,22 +1,32 @@
 package it.unina.softeng.tdd.demo.keepmovies;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.time.Year;
+import java.util.Set;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import static org.hamcrest.MatcherAssert.assertThat; 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
 
+@DisplayName("A KeepMovies")
 class KeepMoviesSpecification {
-	
+
 	void someMoviesInCaseYouNeedThem() {
 		Movie joker, jojo, dunkirk, up;
-		joker   = new Movie("Joker",Year.of(2019),"thriller");
+		joker   = new Movie("Joker", Year.of(2019), "thriller");
 		jojo    = new Movie("Jojo Rabbit", Year.of(2019), "comedy-drama");
-		dunkirk = new Movie("Dunkirk",Year.of(2017),"war");
-		up      = new Movie("Up",Year.of(2009),"comedy-drama");
+		dunkirk = new Movie("Dunkirk", Year.of(2017), "war");
+		up      = new Movie("Up", Year.of(2009), "comedy-drama");
 	}
-	
+
+	@Test
+	void shouldNotContainAnyMovieWhenInstantiated() {
+		KeepMovies keepMovies = new KeepMovies();
+
+		Set<Movie> movies = keepMovies.getMovies();
+
+		assertThat(movies, is(empty()));
+	}
 }
