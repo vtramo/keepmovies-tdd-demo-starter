6b6331458aac9a5617651165c78f127268a01934
Vincenzo Tramo <vv.tramo@gmail.com>
2023-05-07 11:48:58 +0200

    (Feat 1 Iteration 1): make it green

--- src/main/java/it/unina/softeng/tdd/demo/keepmovies/KeepMovies.java
+++ src/main/java/it/unina/softeng/tdd/demo/keepmovies/KeepMovies.java
@@ -1,10 +1,11 @@
 package it.unina.softeng.tdd.demo.keepmovies;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class KeepMovies {
 
   public Set<Movie> getMovies() {
-    return null;
+    return new HashSet<>();
   }
 }
